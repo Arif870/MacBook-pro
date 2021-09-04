73e2bacc67040bdccdaa7e20f5987da9f4afb11b
Arif870 <arifuzzaman959@gmail.com>
2021-09-04 10:00:10 +0600

    setInterval and clear interval

--- timeout.js
+++ timeout.js
@@ -2,3 +2,14 @@ console.log("Happy coding");
 setTimeout(() => {
   console.log("Dont be late");
 }, 5000);
+
+let number = 0;
+
+let val = setInterval(() => {
+  number++;
+  console.log(number);
+
+  if (number === 10) {
+    clearInterval(val);
+  }
+}, 1000);
