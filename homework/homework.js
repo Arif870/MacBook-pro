05ee118e9addfafab410fafd1cd3811add61abba
Arif870 <arifuzzaman959@gmail.com>
2021-09-06 20:13:10 +0600

    event loop

--- homework/homework.js
+++ homework/homework.js
@@ -1,27 +1,14 @@
-// 1.somo win
-
-let n = 10;
-
-function num(n) {
-  n += 100;
-  console.log(n);
-}
-num(n);
-console.log(n);
-
-// 2
-
-let obj = {
-  a: 10,
-  b: 20,
+let second = () => {
+  console.log("second");
 };
 
-function any() {
-  let c = (obj.a = 100);
-  let d = (obj.b = 200);
+let third = () => {
+  setTimeout(second, 5000);
+  console.log("third");
+};
 
-  console.log(c + d);
-}
-any();
-
-console.log(obj);
+let first = () => {
+  third();
+  console.log("first");
+};
+first();
